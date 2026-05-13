7bbc195287b5dd0c47128a9bf502df22f33490aa
salauddinchowdhury736-oss <salauddinchowdhury736@gmail.com>
2026-05-13 21:18:29 +0600

    integrated puter a serverless Ai

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return <main className='bg-[url(public/images/bg-main.svg)] bg-cover'>
  <section className='main-section'>
  <Navbar/>
-<div className="page-heading">
+<div className="page-heading py-18">
   <h1>Track your Applications & Resume Ratings </h1>
   <h2>Review your submissions  and check AI-powered feedback</h2>
 </div>
