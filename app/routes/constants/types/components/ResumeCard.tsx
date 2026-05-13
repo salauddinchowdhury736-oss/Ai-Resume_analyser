189d68483c1dfa508c4ba177d578ffc7e4a2d705
salauddinchowdhury736-oss <salauddinchowdhury736@gmail.com>
2026-05-13 19:26:51 +0600

    resume card & navbar added

---
+++ app/routes/constants/types/components/ResumeCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ResumeCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ResumeCard
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "./Navbar";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,13 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+ <section className='bg-[url(public/images/bg-main.svg)] bg-cover bg-center h-screen flex flex-col items-center justify-center gap-4'>
+ <Navbar/>
+<div className="page-heading">
+  <h1>Track your Applications & Resume Ratings </h1>
+  <h2>Review your submissions  and check AI-powered feedback</h2>
+</div>
+ </section>
+  </main>;
 }
